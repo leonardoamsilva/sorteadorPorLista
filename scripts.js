7258d1d4f7ad99b61376fb0d09821c0862bb3789
Leonardo Anjös <77686897+leonardoamsilva@users.noreply.github.com>
2022-04-22 17:24:21 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const sorteado = document.getElementById("sorteado");
+const arquivo = document.getElementById("arqCsv")
+let lista2 = []
+
+function lerArquivoTxt(evt){
+    var texto = "";
+    var files = evt.target.files;
+    for (var i = 0, f; f = files[i]; i++){
+        var reader = new FileReader();
+        reader.onload = function(event){
+            var conteudo = event.target.result;
+            var linhas = conteudo.split('\n');
+            for(x=0;x<linhas.length;x++){
+                texto += linhas[x];
+            }
+            const res = texto;
+            console.log(texto)
+            let result = res.split(",")
+            console.log(result)
+            lista2 = result;
+            
+        };
+        reader.readAsText(f);
+    }
+}
+document.getElementById('files').addEventListener('change', lerArquivoTxt, false);
+
+function sortear(){
+    numero = Math.round(Math.random()*2)
+    sorte = lista2[numero]
+    sorteado.innerHTML = sorte
+    console.log("Funcionando corretamente")
+    console.log(sorte)
+    console.log(lista2)
+}
